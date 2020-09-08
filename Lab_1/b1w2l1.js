9e98c9024590a5f836b81b706df13e1f7744b229
Damian Vaartmans <99061149@mydavinci.nl>
2020-09-08 11:08:06 +0200

    lab1 gemaakt

---
+++ Lab_1/b1w2l1.js
@@ -0,0 +1,17 @@
+// begin hier je JavaScript commandos
+
+//Damian Vaartmans 99061149
+//Software Developer
+
+
+//zorgt ervoor dat voornaam gevraagt wordt
+var fname = prompt("Wat is je voornaam?");
+
+//zorgt ervoor dat je achternaam gevraagt wordt
+var lname = prompt("Wat is je achternaam?");
+
+//zorgt ervoor dat je leeftijd gevraagt wordt
+var age = prompt("Wat is je leeftijd?");
+
+//zorgt ervoor dat de gegevens op de site worden latenzien
+document.write("jij bent " +fname+ " "+ lname + " " + "en je bent "+ age + " jaar oud");
